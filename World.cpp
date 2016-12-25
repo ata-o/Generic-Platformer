d06f9876032e2b145a258a9ad5f4d618a3a1b442
Onur Ata <onuratasaritas@gmail.com>
2016-12-25 14:27:30 +0200

    Add files via upload

---
+++ World.cpp
@@ -0,0 +1,40 @@
+#include "World.h"
+
+World::World()
+{
+	p[0].setX(20);
+	p[0].setY(500);
+	p[0].setW(100);
+	p[0].setH(40);
+
+	p[1].setX(280);
+	p[1].setY(680);
+	p[1].setW(100);
+	p[1].setH(40);
+
+	p[2].setX(320);
+	p[2].setY(640);
+	p[2].setW(100);
+	p[2].setH(40);
+
+	p[3].setX(640);
+	p[3].setY(560);
+	p[3].setW(100);
+	p[3].setH(40);
+
+	p[4].setX(260);
+	p[4].setY(440);
+	p[4].setW(100);
+	p[4].setH(40);
+
+	p[5].setX(580);
+	p[5].setY(620);
+	p[5].setW(40);
+	p[5].setH(40);
+
+	p[6].setX(0);
+	p[6].setY(720);
+	p[6].setW(1280);
+	p[6].setH(3);
+}
+
